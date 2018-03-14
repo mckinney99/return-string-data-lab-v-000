ae1a2e64916ff808d71fe1dd1797221fb56d7cc2
Eric McKinney <mckinney99@gmail.com>
2018-03-14 01:34:03 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   resources :orders
   resources :invoices
   resources :products, only: [:index, :new, :create, :show]
- 
+
    get '/products/:id/description', to: 'products#description'
    get '/products/:id/inventory', to: 'products#inventory'
-end
+  end
 end
